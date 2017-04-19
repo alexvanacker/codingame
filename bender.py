3f6b9fb156bc3020f36ef2d1244755e53938e7cb
Alex Vanacker <alexvanacker@gmail.com>
2017-04-19 22:56:38 +0200

    First state machine implem
    Ugly as hell, and direct dir changers (N, S, W, E) on map do not
    work as the actions is processed after changing directions.
    Did not handle transporters yet.

--- bender.py
+++ bender.py
@@ -4,6 +4,7 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+
 class Map:
 
     def __init__(self, map_array, l, c):
@@ -20,43 +21,102 @@ class Map:
                 if self.array[x][y] == '@':
                     return (x, y)
 
+    def remove_obstacle(self, x, y):
+        self.array[x][y] = ' '
 
 
 class BenderState:
 
-
     def __init__(self, map):
+        # Should be the same as direction_increment
         self.inversion_activated = False
         self.breaker_mode = False
-        self.directions = ['S', 'E', 'N', 'W']
-        self.current_dir = 'S'
+        self.directions = ['SOUTH', 'EAST', 'NORTH', 'WEST']
+        self.direction_increment = 1
+        self.current_dir = 'SOUTH'
         self.position = map.get_starting_point()
+        self.done = False
 
-    def next_step(self, map):
-        x, y = self.position
-        next_pos_type = None
-        if self.current_dir == 'S':
-            next_pos_type = map.get(x-1, y)
-        if self.current_dir == 'E':
-            next_pos_type = map.get(x, y+1)
-        if self.current_dir == 'N':
-            next_pos_type = map.get(x+1, y)
-        if self.current_dir == 'W':
-            next_pos_type = map.get(x, y-1)
+    def next(self, mmap, (cell_x, cell_y)):
+        ''' Change the state given the cell we go to '''
+        cell_type = mmap.get(cell_x, cell_y)
+        if cell_type == '#':
+            # Increment the direction
+            dir_index = self.directions.index(self.current_dir) + self.direction_increment
+            self.current_dir = self.directions[dir_index]
 
-        if next_pos_type == '#':
-            self.current_dir = directions[]
-        
+        elif cell_type == ' ':
+            #  Move in the current direction
+            self.position = (cell_x, cell_y)
 
-   
+        elif cell_type == '$':
+            self.position = (cell_x, cell_y)
+            self.done = True
 
-def process(map_array):
-    map = Map(map_array)
+        elif cell_type == 'I':
+            # move to the cell
+            self.position = (cell_x, cell_y)
+            # Change the direction increment
+            self.direction_increment = -self.direction_increment
+
+        elif cell_type == 'B':
+            self.position = (cell_x, cell_y)
+            self.breaker_mode = not self.breaker_mode
+        elif cell_type == 'X':
+            if self.breaker_mode:
+                # Break
+                self.position = (cell_x, cell_y)
+                # Remove X from map
+                mmap.remove_obstacle(cell_x, cell_y)
+                pass
+            else:
+                dir_index = self.directions.index(self.current_dir) + self.direction_increment
+                self.current_dir = self.directions[dir_index]
+
+        elif cell_type == 'N':
+            self.position = (cell_x, cell_y)
+            self.current_dir = 'NORTH'
+        elif cell_type == 'S':
+            self.position = (cell_x, cell_y)
+            self.current_dir = 'SOUTH'
+        elif cell_type == 'E':
+            self.position = (cell_x, cell_y)
+            self.current_dir = 'EAST'
+        elif cell_type == 'W':
+            self.position = (cell_x, cell_y)
+            self.current_dir = 'WEST'
+
+        else:
+            raise Exception('Non handled cell type ' + str(cell_type))
+
+        return self
+
+    def run(self, map):
+        actions_array = []
+        while not self.done:
+            x, y = self.position
+            if self.current_dir == 'SOUTH':
+                next_pos = (x + 1, y)
+            if self.current_dir == 'EAST':
+                next_pos = (x, y + 1)
+            if self.current_dir == 'NORTH':
+                next_pos = (x - 1, y)
+            if self.current_dir == 'WEST':
+                next_pos = (x, y - 1)
+
+            self = self.next(map, next_pos)
+            if self.position != (x, y):
+                print >> sys.stderr, 'Moving to {}'.format(str(self.position))
+                print self.current_dir
+                actions_array.append(self.current_dir)
+        return actions_array
 
 
-
-
-
+def process(map_array, lines, columns):
+    mmap = Map(map_array, lines, columns)
+    state = BenderState(mmap)
+    actions = state.run(mmap)
+    return actions
 
 def main():
     l, c = [int(i) for i in raw_input().split()]
@@ -66,7 +126,8 @@ def main():
         row = raw_input()
         print >> sys.stderr, row
         map_array.append(row)
-    print "answer"
+    
+
 
 
 if __name__ == '__main__':
--- bendertest.py
+++ bendertest.py
@@ -1,10 +1,8 @@
 import unittest
-import time
-import sys
 import bender
 
-class Test(unittest.TestCase):
 
+class Test(unittest.TestCase):
 
     def testMapInit(self):
         map_array = ['#####',
@@ -12,8 +10,58 @@ class Test(unittest.TestCase):
                      '#   #',
                      '#   #',
                      '#####']
-        l = 5
-        c = 5
-        map = bender.Map(map_array,l,c)
-        self.assertEquals('@', map.get(1,1))
-        self.assertEquals((1,1), map.get_starting_point())
+        lines = 5
+        columns = 5
+        map = bender.Map(map_array, lines, columns)
+        self.assertEquals('@', map.get(1, 1))
+        self.assertEquals((1, 1), map.get_starting_point())
+
+    def testSimpleSouth(self):
+        map_array = ['#####',
+                     '#@  #',
+                     '#   #',
+                     '#$  #',
+                     '#####']
+        lines = 5
+        columns = 5
+        actions = bender.process(map_array, lines, columns)
+        self.assertEquals(['SOUTH', 'SOUTH'], actions)
+
+    def testSimpleCorner(self):
+        map_array = ['#####',
+                     '#$  #',
+                     '#   #',
+                     '#@  #',
+                     '#####']
+        lines = 5
+        columns = 5
+        actions = bender.process(map_array, lines, columns)
+        self.assertEquals(['EAST', 'EAST',
+                           'NORTH', 'NORTH',
+                           'WEST', 'WEST'], actions)
+
+    def testInverter(self):
+        map_array = ['#####',
+                     '#$  #',
+                     '#  @#',
+                     '#  I#',
+                     '#####']
+        lines = 5
+        columns = 5
+        actions = bender.process(map_array, lines, columns)
+        self.assertEquals(['SOUTH', 'WEST',
+                           'WEST', 'NORTH',
+                           'NORTH'], actions)
+
+    def testExample(self):
+        map_array = ['######',
+                     '#@E $#',
+                     '# N  #',
+                     '#X   #',
+                     '######']
+        lines = 5
+        columns = 6
+        actions = bender.process(map_array, lines, columns)
+        self.assertEquals(['SOUTH', 'EAST',
+                           'NORTH', 'EAST',
+                           'EAST'], actions)

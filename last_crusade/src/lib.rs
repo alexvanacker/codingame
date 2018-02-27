8cca60e95845fb2160368db8e1f79961c06710bd
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-27 13:04:58 +0100

    LastCrusade: Add code from coding game

--- last_crusade/src/lib.rs
+++ last_crusade/src/lib.rs
@@ -1,7 +1,58 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+
+use std::io;
+
+#[derive(Debug)]
+struct Map {
+    width: i32,
+    height: i32
+}
+
+macro_rules! print_err {
+    ($($arg:tt)*) => (
+        {
+            use std::io::Write;
+            writeln!(&mut ::std::io::stderr(), $($arg)*).ok();
+        }
+    )
+}
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+fn main() {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let inputs = input_line.split(" ").collect::<Vec<_>>();
+    let w = parse_input!(inputs[0], i32); // number of columns.
+    let h = parse_input!(inputs[1], i32); // number of rows.
+    for i in 0..h as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let line = input_line.trim_right().to_string(); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
+    }
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let ex = parse_input!(input_line, i32); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
+
+    // game loop
+    loop {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let xi = parse_input!(inputs[0], i32);
+        let yi = parse_input!(inputs[1], i32);
+        let pos = inputs[2].trim().to_string();
+
+        // Write an action using println!("message...");
+        // To debug: print_err!("Debug message...");
+
+
+        // One line containing the X Y coordinates of the room in which you believe Indy will be on the next turn.
+        println!("0 0");
     }
 }

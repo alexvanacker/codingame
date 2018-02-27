5762a428c8758501fe09245d29330d3f351ad655
Alexis Vanacker <alexis.vanacker@concordnow.com>
2018-02-27 14:12:50 +0100

    First working solution
    Needs to be cleaned (or rustified)

--- last_crusade/src/lib.rs
+++ last_crusade/src/lib.rs
@@ -1,12 +1,6 @@
 
 use std::io;
 
-#[derive(Debug)]
-struct Map {
-    width: i32,
-    height: i32
-}
-
 macro_rules! print_err {
     ($($arg:tt)*) => (
         {
@@ -30,11 +24,22 @@ fn main() {
     let inputs = input_line.split(" ").collect::<Vec<_>>();
     let w = parse_input!(inputs[0], i32); // number of columns.
     let h = parse_input!(inputs[1], i32); // number of rows.
+    let mut map: Vec<Vec<i32>> = Vec::with_capacity(h as usize);
+
     for i in 0..h as usize {
         let mut input_line = String::new();
         io::stdin().read_line(&mut input_line).unwrap();
         let line = input_line.trim_right().to_string(); // represents a line in the grid and contains W integers. Each integer represents one room of a given type.
+        let mut row: Vec<i32> = Vec::with_capacity(w as usize);
+
+        for room in line.split(" ").collect::<Vec<_>>(){
+            let room = room.parse::<i32>().unwrap();
+            row.push(room);
+        }
+        map.push(row);
     }
+
+    //print_err!("Map is {:?}", map);
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let ex = parse_input!(input_line, i32); // the coordinate along the X axis of the exit (not useful for this first mission, but must be read).
@@ -48,11 +53,38 @@ fn main() {
         let yi = parse_input!(inputs[1], i32);
         let pos = inputs[2].trim().to_string();
 
+        let room_type = map[yi as usize][xi as usize];
+        print_err!("Room type for ({}, {}): {}", xi, yi, room_type);
+        let (x_exit, y_exit) = get_exit(room_type, xi ,yi, &pos);
         // Write an action using println!("message...");
         // To debug: print_err!("Debug message...");
 
-
         // One line containing the X Y coordinates of the room in which you believe Indy will be on the next turn.
-        println!("0 0");
+        println!("{} {}", x_exit, y_exit);
+    }
+}
+
+pub fn get_exit(room_type: i32, pos_x: i32, pos_y: i32, pos: &String) -> (i32, i32) {
+    match (room_type, pos.as_ref()) {
+        (0, _) => return (-1, -1),
+        (1, _) => return (pos_x, pos_y + 1),
+        (2, "LEFT") => return (pos_x + 1, pos_y),
+        (2, "RIGHT") => return (pos_x - 1, pos_y),
+        (3, _) => (pos_x, pos_y + 1),
+        (4, "TOP") => (pos_x - 1, pos_y),
+        (4, "RIGHT") => (pos_x, pos_y + 1),
+        (5, "TOP") => (pos_x + 1, pos_y),
+        (5, "LEFT") => (pos_x, pos_y + 1),
+        (6, "LEFT") => return (pos_x + 1, pos_y),
+        (6, "RIGHT") => return (pos_x - 1, pos_y),
+        (7, _) => return (pos_x, pos_y + 1),
+        (8, _) => return (pos_x, pos_y + 1),
+        (9, _) => return (pos_x, pos_y + 1),
+        (10, _) => return (pos_x - 1, pos_y),
+        (11, _) => (pos_x + 1, pos_y),
+        (12, _) => (pos_x, pos_y + 1),
+        (13, _) => (pos_x, pos_y + 1),
+        _ => (-1, -1)
+
     }
 }
